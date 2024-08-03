18d983d3268c4332e1d9d2cce8715fd035a0a2b4
wu-yiqiang <wu_yiqiang@outlook.com>
2024-08-04 00:32:49 +0800

    添加功能

--- frontend/src/pages/dashbord/index.tsx
+++ frontend/src/pages/dashbord/index.tsx
@@ -1,5 +1,14 @@
+import '../../assets/styles/dashbord.scss'
 function Dashbord() {
-  return <div className="Dashbord">Dashbord</div>
+  return (
+    <div className="Dashbord">
+      <div className="topbox">
+        <div className="title">状态：运行中</div>
+        <div className="title">版本：1.25.6</div>
+        <div className="menmntroy">内存消耗: 12.34MB</div>
+      </div>
+    </div>
+  )
 }
 
 export default Dashbord
